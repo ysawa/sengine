d0846ddb44f345bf184ce7f926297084a2d11de9
Y Sawa <yoshihirosawa@gmail.com>
2012-02-04 10:58:44 +0900

    add: tests of wav in AudioController

--- spec/controllers/audio_controller_spec.rb
+++ spec/controllers/audio_controller_spec.rb
@@ -3,16 +3,32 @@ require 'spec_helper'
 describe AudioController do
 
   describe "GET 'encode'" do
-    before :each do
-      get 'encode', filename: 'put', format: 'mp3'
+    context 'with mp3 format' do
+      before :each do
+        get 'encode', filename: 'put', format: 'mp3'
+      end
+
+      it "returns http success" do
+        response.should be_success
+      end
+
+      it "render text of encoded Data URI Scheme" do
+        response.body.should match /^data:audio\/mp3/
+      end
     end
 
-    it "returns http success" do
-      response.should be_success
-    end
+    context 'with wav format' do
+      before :each do
+        get 'encode', filename: 'put', format: 'wav'
+      end
 
-    it "render text of encoded Data URI Scheme" do
-      response.body.should match /^data:audio\/mp3/
+      it "returns http success" do
+        response.should be_success
+      end
+
+      it "render text of encoded Data URI Scheme" do
+        response.body.should match /^data:audio\/wav/
+      end
     end
   end
 end
