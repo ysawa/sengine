120fcd3ba14fc9eabf963bd3982a781f811b5898
Y Sawa <yoshihirosawa@gmail.com>
2012-02-06 16:22:05 +0900

    refactor: helper method name

--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 module PiecesHelper
-  def convert_role_to_kanji(role)
+  def convert_piece_role_to_kanji(role)
     I18n.t('piece.roles')[role.to_sym]
   end
 end
--- spec/helpers/pieces_helper_spec.rb
+++ spec/helpers/pieces_helper_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 
 describe PiecesHelper do
-  describe '.convert_role_to_kanji' do
-    it 'convert role name to kanji' do
-      kanji = convert_role_to_kanji('fu')
+  describe '.convert_piece_role_to_kanji' do
+    it 'convert role name of piece to kanji' do
+      kanji = convert_piece_role_to_kanji('fu')
       kanji.should == '歩'
-      kanji = convert_role_to_kanji('kaku')
+      kanji = convert_piece_role_to_kanji('kaku')
       kanji.should == '角'
     end
   end
