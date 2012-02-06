bfaf9d6f138d6fb37fb04fe46ed3ae9a5ce0ba7f
Y Sawa <yoshihirosawa@gmail.com>
2012-02-06 15:58:08 +0900

    remove: unnecessary logger

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -10,8 +10,6 @@ class MovementsController < ApplicationController
     @movement = Movement.new(params[:movement])
     @movement.game = @game
     @game.make_board_from_movement(@movement)
-    p @movement
-    p @game.boards
     respond_to do |format|
       format.js { render text: 'OK', content_type: Mime::TEXT }
       format.html { redirect_to game_url(@game) }
