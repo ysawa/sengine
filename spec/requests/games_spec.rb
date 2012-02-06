f365613ffde1def24e69c78cd442975e3df73fc1
Y Sawa <yoshihirosawa@gmail.com>
2012-02-06 15:58:29 +0900

    add: integration test of games#show

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -22,4 +22,30 @@ describe "Games" do
       end
     end
   end
+
+  describe "GET /games/1" do
+    context 'if user signed in' do
+      before :each do
+        @sente_user = Fabricate(:user)
+        @gote_user = Fabricate(:user)
+        @game = Game.new
+        @game.boards << Board.hirate
+        @game.sente_user = @sente_user
+        @game.gote_user = @gote_user
+      end
+
+      it "works!" do
+        user_sign_in_with_post(@sente_user, 'testtest')
+        get game_path(@game)
+        response.status.should be(200)
+      end
+    end
+
+    context 'if user NOT signed in' do
+      it "doesn't work." do
+        get game_path(@game)
+        response.status.should be(302)
+      end
+    end
+  end
 end
