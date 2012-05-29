dfb304de097bff99cca029298fd1c3a4b74b1186
Y Sawa <yoshihirosawa@gmail.com>
2012-05-30 00:24:58 +0900

    add: attributes from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,13 @@ class User
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable
 
+  field :facebook_id, type: Integer
+  field :facebook_username, type: String
+  field :gender, type: String
+  field :locale, type: String
+  field :name, type: String
+  field :timezone, type: Integer
+
   has_many :sente_games, class_name: 'Game', inverse_of: :sente_user
   has_many :gote_games, class_name: 'Game', inverse_of: :gote_user
   has_many :created_games, class_name: 'Game', inverse_of: :author
@@ -21,17 +28,31 @@ class User
   class << self
     def find_for_facebook_oauth(access_token, signed_in_resource=nil)
       data = access_token.extra.raw_info
-      if user = where(email: data.email).first
-        user
-      else # Create a user with a stub password.
-        create(:email => data.email, :password => Devise.friendly_token[0,20])
+      user = where(email: data.email).first
+      unless user
+        # Create a user with a stub password.
+        user = create(:email => data.email, :password => Devise.friendly_token[0,20])
       end
+      user.facebook_id = data.id
+      user.name = data.name
+      user.facebook_username = data.username
+      user.locale = data.locale
+      user.timezone = data.timezone
+      user.gender = data.gender
+      user.save
+      user
     end
 
     def new_with_session(params, session)
       tap do |user|
         if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+          user.name = data["name"]
+          user.facebook_id = data["id"]
+          user.facebook_username = data["username"]
           user.email = data["email"]
+          user.locale = data["locale"]
+          user.timezone = data["timezone"]
+          user.gender = data["gender"]
         end
       end
     end
