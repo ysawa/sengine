cf3f5991285e914fc06b9a141a3f1485e669593a
Y Sawa <yoshihirosawa@gmail.com>
2012-06-02 01:06:32 +0900

    modify: set facebook_token into session

--- app/controllers/devise_omniauth_callbacks_controller.rb
+++ app/controllers/devise_omniauth_callbacks_controller.rb
@@ -4,9 +4,10 @@ class DeviseOmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+    session[:facebook_token] = request.env["omniauth.auth"].credentials.token.to_s
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Facebook"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
