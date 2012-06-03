a96e322394f355a64c6763448809b3d510ea3ccf
Y Sawa <yoshihirosawa@gmail.com>
2012-06-03 22:51:06 +0900

    fix: @user not current_user

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 
 describe ProfileController do
 
+  before :each do
+    @opponent = Fabricate(:user, name: 'opponent')
+  end
+
   context 'if NOT signed in' do
     describe "GET 'show'" do
       it "returns http redirect" do
-        get 'show'
+        get 'show', id: @opponent.id.to_s
         response.should be_redirect
       end
     end
@@ -15,12 +19,14 @@ describe ProfileController do
 
     before :each do
       user_sign_in
+      @opponent = Fabricate(:user, name: 'opponent')
     end
 
     describe "GET 'show'" do
       it "returns http success" do
-        get 'show'
+        get 'show', id: @opponent.id.to_s
         response.should be_success
+        assigns[:user].should == @opponent
       end
     end
   end
