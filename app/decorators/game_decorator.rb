5513fff4cb1694423d29fe603a93bdb6b7d08b41
Y Sawa <yoshihirosawa@gmail.com>
2012-06-03 21:42:23 +0900

    modify: enable to make comments

--- app/decorators/game_decorator.rb
+++ app/decorators/game_decorator.rb
@@ -3,6 +3,11 @@
 class GameDecorator < ApplicationDecorator
   decorates :game
 
+  def facebook_comments_count
+    url = h.game_url(model, protocol: 'http')
+    "<fb:comments-count href=#{url}></fb:comments-count>".html_safe
+  end
+
   def finished_at(options = {})
     time = model.read_attribute(:finished_at)
     localize_time(time, options)
