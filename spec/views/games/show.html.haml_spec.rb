0132236a947bc933a0b93c2a51de2ebd4b17756c
Y Sawa <yoshihirosawa@gmail.com>
2012-06-10 22:05:53 +0900

    fix: tests

--- spec/views/games/show.html.haml_spec.rb
+++ spec/views/games/show.html.haml_spec.rb
@@ -1,11 +1,24 @@
+# -*- coding: utf-8 -*-
+
 require 'spec_helper'
 
 describe "games/show" do
   before(:each) do
     user_sign_in
-    @game = assign(:game, stub_model(Game))
-    @game.boards << Board.hirate
-    @board = assign(:board, @game.boards.first)
+    @board = Board.hirate
+    @board.game = @game
+    @board.save
+    @sente_user = Fabricate(:user)
+    @gote_user = Fabricate(:user)
+    @game = Game.new
+    @game.sente_user = @sente_user
+    @game.gote_user = @gote_user
+    @game.save
+    assign(:game, @game)
+    @board = Board.hirate
+    @board.game = @game
+    @board.save
+    assign(:board, @board)
   end
 
   it "renders a board with 81 cells" do
