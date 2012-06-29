4bcb64a61c04b0f893673e4ccb64c629d9a6b0e5
Y Sawa <yoshihirosawa@gmail.com>
2012-06-29 23:08:15 +0900

    add: ScoreCalculate#calculate

--- lib/score_calculator.rb
+++ lib/score_calculator.rb
@@ -2,10 +2,42 @@
 
 class ScoreCalculator
   attr_accessor :winner_score, :loser_score
+  attr_reader :winner_next_score, :loser_next_score, :score_change
   GRADE_SCORES = [
        0,  100,  200,  300,  400,  500,  600,  700,  800,  900, 1000, 1100, # Beginner to 5 Kyu
     1200, 1300, 1400, 1500, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000 # 6 Kyu to 8 Dan
   ]
+  SCORE_CHANGES = [
+    [-363, 31], [-338, 30], [-313, 29], [-288, 28], [-263, 27], [-238, 26],
+    [-213, 25], [-188, 24], [-163, 23], [-138, 22], [-113, 21],  [-88, 20],
+     [-63, 19],  [-38, 18],  [-13, 17],   [12, 16],   [37, 15],   [62, 14],
+      [87, 13],  [112, 12],  [137, 11],  [162, 10],   [187, 9],   [212, 8],
+      [237, 7],   [262, 6],   [287, 5],   [312, 4],   [337, 3],   [362, 2],
+      [399, 1]
+  ]
+
+  def calculate
+    @score_difference = @winner_score - @loser_score
+    if @score_difference.abs < 400
+      SCORE_CHANGES.each do |score_change|
+        difference = score_change[0]
+        @score_change = score_change[1]
+        break if @score_difference <= difference
+      end
+    else
+      @score_change = 0
+    end
+    @winner_next_score = @winner_score + @score_change
+    @loser_next_score = @loser_score - @score_change
+  end
+
+  def initialize(winner_score = 0, loser_score = 0)
+    @winner_score = winner_score
+    @loser_score = loser_score
+    @winner_next_score = winner_score
+    @loser_next_score = loser_score
+    @score_change = 0
+  end
 
   class << self
     def grade_to_score(grade)
--- spec/lib/score_calculator_spec.rb
+++ spec/lib/score_calculator_spec.rb
@@ -8,6 +8,65 @@ describe ScoreCalculator do
     @calculator = ScoreCalculator.new
   end
 
+  describe '.calculate' do
+    context 'if difference between scores beyonds 400' do
+      it 'scores do not change' do
+        @calculator.winner_score = 2000
+        @calculator.loser_score = 1000
+        @calculator.calculate
+        @calculator.score_change.should == 0
+        @calculator.winner_next_score.should == 2000
+        @calculator.loser_next_score.should == 1000
+
+        @calculator.winner_score = 1000
+        @calculator.loser_score = 2000
+        @calculator.calculate
+        @calculator.score_change.should == 0
+        @calculator.winner_next_score.should == 1000
+        @calculator.loser_next_score.should == 2000
+      end
+    end
+
+    context 'if difference between scores are enough small' do
+      it 'calculate difference between scores and score changes' do
+        @calculator.winner_score = 1000
+        @calculator.loser_score = 1000
+        @calculator.calculate
+        @calculator.score_change.should == 16
+        @calculator.winner_next_score.should == 1016
+        @calculator.loser_next_score.should == 984
+      end
+
+      it 'when difference is +150, the score change is 10' do
+        @calculator.winner_score = 1150
+        @calculator.loser_score = 1000
+        @calculator.calculate
+        @calculator.score_change.should == 10
+        @calculator.winner_next_score.should == 1160
+        @calculator.loser_next_score.should == 990
+      end
+
+      it 'when diffenrence is -338, the score change is 30' do
+        @calculator.winner_score = 1000
+        @calculator.loser_score = 1338
+        @calculator.calculate
+        @calculator.score_change.should == 30
+        @calculator.winner_next_score.should == 1030
+        @calculator.loser_next_score.should == 1308
+      end
+
+      it 'when diffenrence is -337, the score change is 29' do
+        @calculator.winner_score = 1000
+        @calculator.loser_score = 1337
+        @calculator.calculate
+        @calculator.score_change.should == 29
+        @calculator.winner_next_score.should == 1029
+        @calculator.loser_next_score.should == 1308
+      end
+
+    end
+  end
+
   describe 'ScoreCalculator.grade_to_score' do
     it 'generates score from grade' do
       ScoreCalculator.grade_to_score(0).should == 0
