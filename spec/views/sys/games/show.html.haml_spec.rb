e8e579dec0757622c335a45de7cd53851e3ad3e1
Y Sawa <yoshihirosawa@gmail.com>
2012-07-03 23:05:44 +0900

    fix: tests

--- spec/views/sys/games/show.html.haml_spec.rb
+++ spec/views/sys/games/show.html.haml_spec.rb
@@ -5,9 +5,21 @@ require 'spec_helper'
 describe "sys/games/show" do
   before :each do
     setup_controller_request
-    @game = Fabricate(:game)
     user_sign_in
+    @board = Board.hirate
+    @board.game = @game
+    @board.save
+    @sente_user = Fabricate(:user)
+    @gote_user = Fabricate(:user)
+    @game = Game.new
+    @game.sente_user = @sente_user
+    @game.gote_user = @gote_user
+    @game.save
     assign(:game, @game)
+    @board = Board.hirate
+    @board.game = @game
+    @board.save
+    assign(:board, @board)
   end
 
   it "renders sys/games/show" do
