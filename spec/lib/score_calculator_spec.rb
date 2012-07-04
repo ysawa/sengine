aa6bee1f419e569f5ef3632703130808d1dfbb2d
Y Sawa <yoshihirosawa@gmail.com>
2012-07-04 20:44:12 +0900

    modify: score * 10

--- spec/lib/score_calculator_spec.rb
+++ spec/lib/score_calculator_spec.rb
@@ -9,67 +9,67 @@ describe ScoreCalculator do
   end
 
   describe '.calculate' do
-    context 'if difference between scores beyonds 400' do
+    context 'if difference between scores beyonds 4000' do
       it 'scores do not change' do
-        @calculator.winner_score = 2000
-        @calculator.loser_score = 1000
+        @calculator.winner_score = 20000
+        @calculator.loser_score = 10000
         @calculator.calculate
         @calculator.score_change.should == 0
-        @calculator.winner_next_score.should == 2000
-        @calculator.loser_next_score.should == 1000
+        @calculator.winner_next_score.should == 20000
+        @calculator.loser_next_score.should == 10000
 
-        @calculator.winner_score = 1000
-        @calculator.loser_score = 2000
+        @calculator.winner_score = 10000
+        @calculator.loser_score = 20000
         @calculator.calculate
         @calculator.score_change.should == 0
-        @calculator.winner_next_score.should == 1000
-        @calculator.loser_next_score.should == 2000
+        @calculator.winner_next_score.should == 10000
+        @calculator.loser_next_score.should == 20000
       end
     end
 
     context 'if difference between scores are enough small' do
       it 'calculate difference between scores and score changes' do
-        @calculator.winner_score = 1000
-        @calculator.loser_score = 1000
+        @calculator.winner_score = 10000
+        @calculator.loser_score = 10000
         @calculator.calculate
-        @calculator.score_change.should == 16
-        @calculator.winner_next_score.should == 1016
-        @calculator.loser_next_score.should == 984
+        @calculator.score_change.should == 160
+        @calculator.winner_next_score.should == 10160
+        @calculator.loser_next_score.should == 9840
       end
 
-      it 'when difference is +150, the score change is 10' do
-        @calculator.winner_score = 1150
-        @calculator.loser_score = 1000
+      it 'when difference is +1500, the score change is 100' do
+        @calculator.winner_score = 11500
+        @calculator.loser_score = 10000
         @calculator.calculate
-        @calculator.score_change.should == 10
-        @calculator.winner_next_score.should == 1160
-        @calculator.loser_next_score.should == 990
+        @calculator.score_change.should == 100
+        @calculator.winner_next_score.should == 11600
+        @calculator.loser_next_score.should == 9900
       end
 
-      it 'when diffenrence is -338, the score change is 30' do
-        @calculator.winner_score = 1000
-        @calculator.loser_score = 1338
+      it 'when diffenrence is -3380, the score change is 300' do
+        @calculator.winner_score = 10000
+        @calculator.loser_score = 13380
         @calculator.calculate
-        @calculator.score_change.should == 30
-        @calculator.winner_next_score.should == 1030
-        @calculator.loser_next_score.should == 1308
+        @calculator.score_change.should == 300
+        @calculator.winner_next_score.should == 10300
+        @calculator.loser_next_score.should == 13080
       end
 
-      it 'when diffenrence is -337, the score change is 29' do
-        @calculator.winner_score = 1000
-        @calculator.loser_score = 1337
+      it 'when diffenrence is -3370, the score change is 290' do
+        @calculator.winner_score = 10000
+        @calculator.loser_score = 13370
         @calculator.calculate
-        @calculator.score_change.should == 29
-        @calculator.winner_next_score.should == 1029
-        @calculator.loser_next_score.should == 1308
+        @calculator.score_change.should == 290
+        @calculator.winner_next_score.should == 10290
+        @calculator.loser_next_score.should == 13080
       end
 
       it 'score should not be below 0' do
         @calculator.winner_score = 0
         @calculator.loser_score = 0
         @calculator.calculate
-        @calculator.score_change.should == 16
-        @calculator.winner_next_score.should == 16
+        @calculator.score_change.should == 160
+        @calculator.winner_next_score.should == 160
         @calculator.loser_next_score.should == 0
       end
     end
@@ -78,19 +78,19 @@ describe ScoreCalculator do
   describe 'ScoreCalculator.grade_to_score' do
     it 'generates score from grade' do
       ScoreCalculator.grade_to_score(0).should == 0
-      ScoreCalculator.grade_to_score(1).should == 100
-      ScoreCalculator.grade_to_score(23).should == 3000
+      ScoreCalculator.grade_to_score(1).should == 1000
+      ScoreCalculator.grade_to_score(23).should == 30000
     end
   end
 
   describe 'ScoreCalculator.score_to_grade' do
     it 'generates grade from score' do
       ScoreCalculator.score_to_grade(0).should == 0
-      ScoreCalculator.score_to_grade(49).should == 0
-      ScoreCalculator.score_to_grade(50).should == 1
-      ScoreCalculator.score_to_grade(100).should == 1
-      ScoreCalculator.score_to_grade(3000).should == 23
-      ScoreCalculator.score_to_grade(4000).should == 23
+      ScoreCalculator.score_to_grade(490).should == 0
+      ScoreCalculator.score_to_grade(500).should == 1
+      ScoreCalculator.score_to_grade(1000).should == 1
+      ScoreCalculator.score_to_grade(30000).should == 23
+      ScoreCalculator.score_to_grade(40000).should == 23
     end
   end
 end
