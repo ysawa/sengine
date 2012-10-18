76182cc8d3d10d50a6f0f8209f23d0a0471dcaa3
Y Sawa <yoshihirosawa@gmail.com>
2012-10-18 23:14:25 +0900

    fix: tests

--- spec/views/about/game.html.haml_spec.rb
+++ spec/views/about/game.html.haml_spec.rb
@@ -6,18 +6,8 @@ describe "about/game" do
   before :each do
   end
 
-  it "renders about/us that is the first explanation for visitors" do
+  it "renders the first explanation for visitors" do
     render
-    rendered.should include "<h3>\nShall we play Shogi with your facebook friends?\n</h3>"
-  end
-
-  context 'when locale is ja' do
-    before :each do
-    end
-
-    it "renders about/game" do
-      render partial: 'about/game.ja'
-      rendered.should include "<h3>\nFacebookの友達と将棋を指そう!\n</h3>"
-    end
+    rendered.should include "<h3>\nFacebookの友達と将棋を指そう!\n</h3>"
   end
 end
--- spec/views/help/index.html.haml_spec.rb
+++ spec/views/help/index.html.haml_spec.rb
@@ -7,20 +7,8 @@ describe "help/index" do
     user_sign_in
   end
 
-  context 'when locale is en' do
-    it "renders help/index" do
-      render
-      rendered.should match "<h3>\nAt first\n</h3>"
-    end
-  end
-
-  context 'when locale is ja' do
-    before :each do
-    end
-
-    it "renders help/index" do
-      render partial: 'help/index.ja'
-      rendered.should match "<h3>\nはじめに\n</h3>"
-    end
+  it "renders help/index" do
+    render
+    rendered.should match "<h3>\nはじめに\n</h3>"
   end
 end
