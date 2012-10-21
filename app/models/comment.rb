b08e67ac05913c97c62cb339deb2c1b03be66182
Y Sawa <yoshihirosawa@gmail.com>
2012-10-22 08:52:55 +0900

    modify: top page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,24 @@ class Comment
   field :content, type: String
   belongs_to :author, class_name: 'User'
   belongs_to :game
+
+  class << self
+    def of_game(game)
+      case game
+      when Array
+        game_ids = game.collect do |g|
+          if g.is_a? Game
+            g.id
+          else
+            Moped::BSON::ObjectId(g)
+          end
+        end
+        criteria.where(:game_id.in => game_ids)
+      when Moped::BSON::ObjectId, String
+        criteria.where(game_id: Moped::BSON::ObjectId(game))
+      else
+        criteria.where(game_id: game.id)
+      end
+    end
+  end
 end
