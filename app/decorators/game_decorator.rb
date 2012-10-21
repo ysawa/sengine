2e2c61770bc25a0fe7ccf3f3848b66e9deef4870
Y Sawa <yoshihirosawa@gmail.com>
2012-10-21 22:59:35 +0900

    modify: game list

--- app/decorators/game_decorator.rb
+++ app/decorators/game_decorator.rb
@@ -12,4 +12,13 @@ class GameDecorator < ApplicationDecorator
     time = model.read_attribute(:finished_at)
     localize_time(time, options)
   end
+
+  def time_span
+    if model.finished_at
+      time = model.finished_at
+    else
+      time = Time.now
+    end
+    h.distance_of_time_in_words(time, model.created_at)
+  end
 end
