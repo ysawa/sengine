9070a82b1532f3da3e027710a2e3c0944296f4e7
Y Sawa <yoshihirosawa@gmail.com>
2012-11-12 13:32:55 +0900

    refactor: devise controller

--- app/controllers/devise_sessions_controller.rb
+++ app/controllers/devise_sessions_controller.rb
@@ -1,27 +1,15 @@
 # -*- coding: utf-8 -*-
 
 class DeviseSessionsController < Devise::SessionsController
-  prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
-  prepend_before_filter :allow_params_authentication!, :only => :create
-
-  # GET /resource/sign_in
-  def new
-    if Rails.env.production?
-      # In production, forbid signing in by email and password.
-      redirect_to user_omniauth_authorize_path(:facebook)
-    else
-      resource = build_resource(nil, :unsafe => true)
-      clean_up_passwords(resource)
-      respond_with(resource, serialize_options(resource))
-    end
-  end
+  prepend_before_filter :require_no_authentication, only: [ :new, :create ]
+  prepend_before_filter :allow_params_authentication!, only: :create
 
   # POST /resource/sign_in
   def create
     resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
-    respond_with resource, :location => after_sign_in_path_for(resource)
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
 
   # DELETE /resource/sign_out
@@ -40,16 +28,28 @@ class DeviseSessionsController < Devise::SessionsController
     end
   end
 
-  protected
+  # GET /resource/sign_in
+  def new
+    if Rails.env.production?
+      # In production, forbid signing in by email and password.
+      redirect_to user_omniauth_authorize_path(:facebook)
+    else
+      resource = build_resource(nil, unsafe: true)
+      clean_up_passwords(resource)
+      respond_with(resource, serialize_options(resource))
+    end
+  end
+
+protected
+
+  def auth_options
+    { scope: resource_name, recall: "#{controller_path}#new" }
+  end
 
   def serialize_options(resource)
     methods = resource_class.authentication_keys.dup
     methods = methods.keys if methods.is_a?(Hash)
     methods << :password if resource.respond_to?(:password)
-    { :methods => methods, :only => [:password] }
-  end
-
-  def auth_options
-    { :scope => resource_name, :recall => "#{controller_path}#new" }
+    { methods: methods, only: [:password] }
   end
 end
