386c68c7fae2233abc4e898dd80a177efd0cf3a3
Y Sawa <yoshihirosawa@gmail.com>
2012-12-13 18:12:00 +0900

    add: tests for tags to search self

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -14,32 +14,56 @@ describe Tag do
     end
   end
 
-  describe 'localization of' do
+  describe 'about localization of' do
     describe 'name' do
-      it 'name can take some locales' do
+      before :each do
         I18n.locale = :en
         @tag.name = 'Tag name en'
         I18n.locale = :ja
         @tag.name = 'Tag name ja'
         @tag.save
+      end
+
+      it 'name can take some locales' do
         I18n.locale = :en
         @tag.name.should == 'Tag name en'
         I18n.locale = :ja
         @tag.name.should == 'Tag name ja'
       end
+
+      it 'tags can be searched' do
+        I18n.locale = :en
+        Tag.where(name: 'Tag name en').first.should == @tag
+        Tag.where(name: 'Tag name ja').first.should be_nil
+        I18n.locale = :ja
+        Tag.where(name: 'Tag name ja').first.should == @tag
+        Tag.where(name: 'Tag name en').first.should be_nil
+      end
     end
 
     describe 'content' do
-      it 'content can take some locales' do
+      before :each do
         I18n.locale = :en
         @tag.content = 'Tag content en'
         I18n.locale = :ja
         @tag.content = 'Tag content ja'
         @tag.save
+      end
+
+      it 'content can take some locales' do
         I18n.locale = :en
         @tag.content.should == 'Tag content en'
         I18n.locale = :ja
         @tag.content.should == 'Tag content ja'
+      end
+
+      it 'tags can be searched' do
+        I18n.locale = :en
+        Tag.where(content: 'Tag content en').first.should == @tag
+        Tag.where(content: 'Tag content ja').first.should be_nil
+        I18n.locale = :ja
+        Tag.where(content: 'Tag content ja').first.should == @tag
+        Tag.where(content: 'Tag content en').first.should be_nil
       end
     end
   end
