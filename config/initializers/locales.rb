ce7c69add4c9b5262a25f1d7e9f0aad015b72541
Y Sawa <yoshihirosawa@gmail.com>
2012-12-14 17:29:18 +0900

    modify: initializing locales

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
 module Sengine
-  LOCALES = [:ja, :en]
+  locales = []
+  Dir.entries(File.join(Rails.root, 'config/locales')).each do |entry|
+    next if entry == '.' || entry == '..'
+    locales << entry.to_sym
+  end
+  LOCALES = locales
 end
