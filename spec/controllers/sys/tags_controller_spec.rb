a443e49bfe98e4bd115bcea9cfd1afcc8f276065
Y Sawa <yoshihirosawa@gmail.com>
2012-12-14 11:50:33 +0900

    add: tests about sys/tags#create, sys/tags#update

--- spec/controllers/sys/tags_controller_spec.rb
+++ spec/controllers/sys/tags_controller_spec.rb
@@ -4,6 +4,11 @@ require 'spec_helper'
 
 describe Sys::TagsController do
 
+  def valid_attributes(attributes = {})
+    attrs = { code: 'tag_code', name: 'Tag Name', content: 'Tag Content' }
+    attrs.merge(attributes)
+  end
+
   before :each do
     @tag = Fabricate(:tag)
   end
@@ -69,6 +74,44 @@ describe Sys::TagsController do
       it 'build new tag' do
         get 'new'
         assigns[:tag].persisted?.should be_false
+      end
+    end
+
+    describe "POST 'create'" do
+      context 'with valid attributes' do
+        it "returns http redirect" do
+          post 'create', { 'tag' => valid_attributes }
+          response.should be_redirect
+        end
+
+        it "creates new tag" do
+          Tag.count.should == 1
+          post 'create', { 'tag' => valid_attributes }
+          Tag.count.should == 2
+        end
+      end
+
+      context 'with invalid attributes' do
+        it "renders edit" do
+          post 'create', { 'tag' => valid_attributes(code: '') }
+          response.should render_template(:edit)
+        end
+      end
+    end
+
+    describe "PUT 'update'" do
+      context 'with valid attributes' do
+        it "returns http redirect" do
+          put 'update', { 'id' => @tag.to_param, 'tag' => valid_attributes }
+          response.should be_redirect
+        end
+      end
+
+      context 'with invalid attributes' do
+        it "renders edit" do
+          put 'update', { 'id' => @tag.to_param, 'tag' => valid_attributes(code: '') }
+          response.should render_template(:edit)
+        end
       end
     end
   end
