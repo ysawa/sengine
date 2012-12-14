b19e1d86e6e1b1f9a4544e20da23f5543afd4099
Y Sawa <yoshihirosawa@gmail.com>
2012-12-14 13:38:28 +0900

    modify: tests for profile/show

--- spec/views/profile/show.html.haml_spec.rb
+++ spec/views/profile/show.html.haml_spec.rb
@@ -5,10 +5,12 @@ require 'spec_helper'
 describe "profile/show" do
   before :each do
     user_sign_in
-    assign(:user, Fabricate(:user, name: 'opponent'))
+    @user = Fabricate(:user, name: 'opponent')
+    assign(:user, @user)
   end
 
-  it "renders profile/show" do
+  it "renders successfully" do
     render
+    rendered.should match @user.name
   end
 end
