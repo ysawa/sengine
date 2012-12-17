959ae1ad6e674f47c45b90999786be98c07a41e0
Y Sawa <yoshihirosawa@gmail.com>
2012-12-17 18:14:11 +0900

    add: tests for view to sign in

---
+++ spec/views/devise/sessions/new.html.haml_spec.rb
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+require 'spec_helper'
+
+describe "devise/sessions/new.html.haml" do
+  before :each do
+    @user = Fabricate(:user)
+    view.stub!(:resource).and_return(User.new)
+    view.stub!(:resource_name).and_return('user')
+    view.stub!(:resource_class).and_return(User)
+    view.stub!(:devise_mapping).and_return(Devise.mappings[:user])
+  end
+
+  it 'renders successfully.' do
+    render
+    rendered.should have_selector 'form#user_sign_in'
+  end
+end
