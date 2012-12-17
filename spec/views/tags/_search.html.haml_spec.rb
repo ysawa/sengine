9cb1ce009069f6d12647f2c3b92622d26f3f24a8
Y Sawa <yoshihirosawa@gmail.com>
2012-12-17 16:49:08 +0900

    add: tests for search form

--- spec/views/tags/_search.html.haml_spec.rb
+++ spec/views/tags/_search.html.haml_spec.rb
@@ -7,11 +7,13 @@ describe "tags/_search.html.haml" do
   before :each do
     @tag = Fabricate(:tag)
     assign(:tags, Tag.page(1))
+    view.stub!(:params).and_return({ q: 'Key Word' })
   end
 
   it 'renders successfully' do
     render
     rendered.should have_selector 'form#search_tag'
     rendered.should have_selector 'input[name="q"]'
+    rendered.should match 'Key Word'
   end
 end
