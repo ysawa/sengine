f3ba5f757c256c1c469faa6470911f12c557e124
Y Sawa <yoshihirosawa@gmail.com>
2012-12-18 11:31:30 +0900

    add: followed users and following users into profile#show

--- spec/views/profile/show.html.haml_spec.rb
+++ spec/views/profile/show.html.haml_spec.rb
@@ -5,12 +5,40 @@ require 'spec_helper'
 describe "profile/show" do
   before :each do
     user_sign_in
-    @user = Fabricate(:user, name: 'opponent')
-    assign(:user, @user)
+    @target = Fabricate(:user, name: 'Target User')
+    assign(:user, @target)
   end
 
   it "renders successfully" do
     render
-    rendered.should match @user.name
+    rendered.should match @target.name
+  end
+
+  describe 'following_users' do
+
+    before :each do
+      @target.follow!(@user)
+      render
+    end
+
+    it 'shows following users' do
+      assert_select '#following_users' do
+        assert_select 'img.face', 1
+      end
+    end
+  end
+
+  describe 'followed_users' do
+
+    before :each do
+      @user.follow!(@target)
+      render
+    end
+
+    it 'shows followed users' do
+      assert_select '#followed_users' do
+        assert_select 'img.face', 1
+      end
+    end
   end
 end
