05c5e96930feecd7f6ac64c6a9c99095fe4adac0
Y Sawa <yoshihirosawa@gmail.com>
2012-12-18 09:49:46 +0900

    modify: tests for sys/home/index

--- spec/views/sys/home/index.html.haml_spec.rb
+++ spec/views/sys/home/index.html.haml_spec.rb
@@ -9,4 +9,13 @@ describe "sys/home/index" do
   it "renders sys/home/index" do
     render template: 'sys/home/index', layout: 'layouts/sys/application'
   end
+
+  it "renders menu" do
+    render template: 'sys/home/index', layout: 'layouts/sys/application'
+    rendered.should have_selector 'a[href="/sys/users"]'
+    rendered.should have_selector 'a[href="/sys/games"]'
+    rendered.should have_selector 'a[href="/sys/feedbacks"]'
+    rendered.should have_selector 'a[href="/sys/tags"]'
+    rendered.should have_selector 'a[href="/sys/users"]'
+  end
 end
