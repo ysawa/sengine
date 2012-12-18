1bea742ac84cd107669500a215b26c7277b75dfb
Y Sawa <yoshihirosawa@gmail.com>
2012-12-18 13:36:58 +0900

    modify: tests for settings_controller more specifically

--- spec/controllers/setting_controller_spec.rb
+++ spec/controllers/setting_controller_spec.rb
@@ -6,16 +6,16 @@ describe SettingController do
 
   context 'if NOT signed in' do
     describe "GET 'edit'" do
-      it "returns http redirect" do
+      it "returns http redirect to new user session" do
         get 'edit'
-        response.should be_redirect
+        response.should redirect_to(new_user_session_path)
       end
     end
 
     describe "PUT 'update'" do
-      it "returns http redirect" do
+      it "returns http redirect to new user session" do
         put 'update'
-        response.should be_redirect
+        response.should redirect_to(new_user_session_path)
       end
     end
   end
@@ -35,8 +35,14 @@ describe SettingController do
 
     describe "PUT 'update'" do
       it "returns http redirect" do
-        put 'update', {}
-        response.should be_redirect
+        put 'update', { user: { name: 'New Name' } }
+        response.should redirect_to(root_path)
+      end
+
+      it "returns http redirect" do
+        put 'update', { user: { name: 'New Name' } }
+        @user.reload
+        @user.name.should == 'New Name'
       end
     end
   end
