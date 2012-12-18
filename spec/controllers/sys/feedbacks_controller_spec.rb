fd5041cfeb0d8a5b06747f7a7768a706b22f0e26
Y Sawa <yoshihirosawa@gmail.com>
2012-12-18 13:52:22 +0900

    modify: tests for sys controllers

--- spec/controllers/sys/feedbacks_controller_spec.rb
+++ spec/controllers/sys/feedbacks_controller_spec.rb
@@ -4,14 +4,27 @@ describe Sys::FeedbacksController do
   before :each do
     @feedback = Fabricate(:feedback)
     @user = Fabricate(:user, admin: true)
-    user_sign_in(@user)
   end
 
+  context 'without signing in as an admin'do
+    describe "GET 'index'" do
+      it "returns http redirect to new user session" do
+        get 'index'
+        response.should redirect_to(new_user_session_path)
+      end
+    end
+  end
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  context 'with signing in as an admin'do
+    before :each do
+      user_sign_in(@user)
+    end
+
+    describe "GET 'index'" do
+      it "returns http success" do
+        get 'index'
+        response.should be_success
+      end
     end
   end
 end
--- spec/controllers/sys/games_controller_spec.rb
+++ spec/controllers/sys/games_controller_spec.rb
@@ -4,27 +4,42 @@ describe Sys::GamesController do
   before :each do
     @game = Fabricate(:game)
     @user = Fabricate(:user, admin: true)
-    user_sign_in(@user)
   end
 
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit', id: @game.to_param
-      response.should be_success
+  context 'without signing in as an admin'do
+    describe "GET 'index'" do
+      it "returns http redirect to new user session" do
+        get 'index'
+        response.should redirect_to(new_user_session_path)
+      end
     end
   end
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
+  context 'with signing in as an admin'do
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show', id: @game.to_param
-      response.should be_success
+    before :each do
+      user_sign_in(@user)
+    end
+
+    describe "GET 'edit'" do
+      it "returns http success" do
+        get 'edit', id: @game.to_param
+        response.should be_success
+      end
+    end
+
+    describe "GET 'index'" do
+      it "returns http success" do
+        get 'index'
+        response.should be_success
+      end
+    end
+
+    describe "GET 'show'" do
+      it "returns http success" do
+        get 'show', id: @game.to_param
+        response.should be_success
+      end
     end
   end
 end
