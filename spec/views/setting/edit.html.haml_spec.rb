25512fe8b20cc20247ce0600596db8befef556cd
Y Sawa <yoshihirosawa@gmail.com>
2012-12-19 10:15:32 +0900

    modify: setting form

--- spec/views/setting/edit.html.haml_spec.rb
+++ spec/views/setting/edit.html.haml_spec.rb
@@ -11,6 +11,12 @@ describe "setting/edit" do
     render
   end
 
+  it 'renders nav tabs, but has no active tab' do
+    render
+    rendered.should have_selector 'ul.nav.nav-tabs li'
+    rendered.should_not have_selector 'ul.nav.nav-tabs li.active'
+  end
+
   context 'editing objective default' do
     before :each do
       @objective = 'default'
@@ -21,6 +27,17 @@ describe "setting/edit" do
       render
       rendered.should have_selector 'form.edit_user'
       rendered.should match @user.name
+    end
+
+    it 'renders the right objective field' do
+      render
+      rendered.should have_selector 'input[name="objective"][value="default"][type="hidden"]'
+    end
+
+    it 'renders nav tabs, but has active tab' do
+      render
+      rendered.should have_selector 'ul.nav.nav-tabs a'
+      rendered.should have_selector 'ul.nav.nav-tabs li.active'
     end
   end
 end
