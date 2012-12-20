02a54dbaabc9f7a7520ccc5707367095d16ceb4d
Y Sawa <yoshihirosawa@gmail.com>
2012-12-20 18:49:54 +0900

    add: tests for Visibility::Shown

--- spec/lib/visibility/shown_spec.rb
+++ spec/lib/visibility/shown_spec.rb
@@ -10,6 +10,7 @@ describe Visibility::Shown do
       include Visibility::Shown
     end
     @user = Fabricate(:user)
+    @another = Fabricate(:user)
   end
 
   describe '.hide_user' do
@@ -17,6 +18,36 @@ describe Visibility::Shown do
       model = TestModel.new
       model.hide_user(@user)
       model.hidden_user_ids.should include @user.id
+    end
+  end
+
+  describe '.hidden?' do
+    before :each do
+      @model = TestModel.new
+      @model.hidden_user_ids << @user.id
+    end
+
+    it 'gets true if the user is hidden' do
+      @model.hidden?(@user).should be_true
+    end
+
+    it 'gets false if the user is shown' do
+      @model.hidden?(@another).should be_false
+    end
+  end
+
+  describe '.shown?' do
+    before :each do
+      @model = TestModel.new
+      @model.hidden_user_ids << @user.id
+    end
+
+    it 'gets true if the user is shown' do
+      @model.shown?(@another).should be_true
+    end
+
+    it 'gets false if the user is hidden' do
+      @model.shown?(@user).should be_false
     end
   end
 end
