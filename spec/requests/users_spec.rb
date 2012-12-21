3db4adcb12d07f28d7d500ad8727c871df744837
Y Sawa <yoshihirosawa@gmail.com>
2012-12-21 15:02:18 +0900

    add: integration tests for signing in

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+require 'spec_helper'
+
+describe 'users' do
+
+  before :each do
+    @user = Fabricate(:user)
+  end
+
+  context 'signing in' do
+    before :each do
+    end
+
+    describe "accessing /users/sign_in" do
+      it "page can be accessed" do
+        visit new_user_session_path
+      end
+
+      it "works successfully with correct password", js: true do
+        user_sign_in_with_visit(@user, 'PASSWORD')
+        page.should_not have_selector 'form#user_sign_in'
+        page.should have_content I18n.t('pages.controllers.home.mypage')
+      end
+
+      it "fails with incorrect password", js: true do
+        user_sign_in_with_visit(@user, 'BAD PASSWORD')
+        page.should have_selector 'form#user_sign_in'
+        page.should_not have_content I18n.t('pages.controllers.home.mypage')
+      end
+    end
+  end
+end
