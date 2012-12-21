de414d41d2141851e0d367587256ccf904dd5df7
Y Sawa <yoshihirosawa@gmail.com>
2012-12-21 10:54:36 +0900

    add: tests for Visibility::Filter

--- app/lib/visibility/filter.rb
+++ app/lib/visibility/filter.rb
@@ -4,7 +4,7 @@ module Visibility::Filter
 
   def black_user?(user)
     user_id = Visibility.get_user_id user
-    use_black_list? && self.black_user_ids.include?(user_id)
+    using_black_list? && self.black_user_ids.include?(user_id)
   end
 
   def visible?(user)
@@ -15,23 +15,24 @@ module Visibility::Filter
 
   def white_user?(user)
     user_id = Visibility.get_user_id user
-    !use_white_list? || self.white_user_ids.include?(user_id)
+    !using_white_list? || self.white_user_ids.include?(user_id)
   end
 
   def self.included(klass)
     klass.field :black_user_ids, type: Array, default: []
-    klass.field :use_black_list, type: Boolean, default: false
-    klass.field :use_white_list, type: Boolean, default: false
+    klass.field :using_black_list, type: Boolean, default: false
+    klass.field :using_white_list, type: Boolean, default: false
     klass.field :white_user_ids, type: Array, default: []
+    klass.attr_protected :black_user_ids, :using_black_list, :using_white_list, :white_user_ids
 
     klass.class_eval <<-EOS
       class << self
         def visible(user)
           user_id = Visibility.get_user_id user
           or_conditions = []
-          or_conditions << { :use_black_list.ne => true, :use_white_list.ne => true }
-          or_conditions << { :use_black_list => true, :black_user_ids.ne => user_id }
-          or_conditions << { :use_white_list => true, :white_user_ids => user_id }
+          or_conditions << { :using_black_list.ne => true, :using_white_list.ne => true }
+          or_conditions << { :using_black_list => true, :black_user_ids.ne => user_id }
+          or_conditions << { :using_white_list => true, :white_user_ids => user_id }
           criteria.where(:$and => [{ :$or => or_conditions }])
         end
       end
--- spec/lib/visibility/filter_spec.rb
+++ spec/lib/visibility/filter_spec.rb
@@ -12,4 +12,26 @@ describe Visibility::Filter do
     @user = Fabricate(:user)
     @another = Fabricate(:user)
   end
+
+  describe '.black_user?' do
+
+    it 'checks if the user is black' do
+      model = TestModel.new
+      model.using_black_list = true
+      model.black_user_ids << @user.id
+      model.black_user?(@user).should be_true
+      model.black_user?(@another).should be_false
+    end
+  end
+
+  describe '.white_user?' do
+
+    it 'checks if the user is white' do
+      model = TestModel.new
+      model.using_white_list = true
+      model.white_user_ids << @user.id
+      model.white_user?(@user).should be_true
+      model.white_user?(@another).should be_false
+    end
+  end
 end
