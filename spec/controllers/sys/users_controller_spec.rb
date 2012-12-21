3bc135978c3fe420b235d4b8f02bb96d357af6e1
Y Sawa <yoshihirosawa@gmail.com>
2012-12-21 15:57:23 +0900

    add: tests for /sys/users

--- spec/controllers/sys/users_controller_spec.rb
+++ spec/controllers/sys/users_controller_spec.rb
@@ -2,17 +2,8 @@ require 'spec_helper'
 
 describe Sys::UsersController do
 
-  context 'with signing in as an admin'do
-    before :each do
-      @user = Fabricate(:user, admin: true)
-      user_sign_in(@user)
-    end
-    describe "GET 'index'" do
-      it "returns http success" do
-        get 'index'
-        response.should be_success
-      end
-    end
+  before :each do
+    @user = Fabricate(:user, admin: true)
   end
 
   context 'without signing in as an admin'do
@@ -20,6 +11,33 @@ describe Sys::UsersController do
       it "returns http redirect to new user session" do
         get 'index'
         response.should redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  context 'with signing in as an admin'do
+    before :each do
+      user_sign_in(@user)
+    end
+
+    describe "GET 'index'" do
+      it "returns http success" do
+        get 'index'
+        response.should be_success
+      end
+    end
+
+    describe "GET 'show'" do
+      it "returns http success" do
+        get 'show', { id: @user.id }
+        response.should be_success
+      end
+    end
+
+    describe "GET 'edit'" do
+      it "returns http success" do
+        get 'edit', { id: @user.id }
+        response.should be_success
       end
     end
   end
