f6e013411119ed96bcce4bcdd18cd87236e39eb1
Y Sawa <yoshihirosawa@gmail.com>
2012-12-21 15:15:54 +0900

    fix: tests in webkit

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -13,20 +13,20 @@ describe 'users' do
     end
 
     describe "accessing /users/sign_in" do
+
       it "page can be accessed" do
         visit new_user_session_path
+        page.should have_selector 'form#user_sign_in'
       end
 
       it "works successfully with correct password", js: true do
         user_sign_in_with_visit(@user, 'PASSWORD')
-        page.should_not have_selector 'form#user_sign_in'
         page.should have_content I18n.t('pages.controllers.home.mypage')
       end
 
       it "fails with incorrect password", js: true do
         user_sign_in_with_visit(@user, 'BAD PASSWORD')
         page.should have_selector 'form#user_sign_in'
-        page.should_not have_content I18n.t('pages.controllers.home.mypage')
       end
     end
   end
