e4845025c1eab9a808c8f151d4c1bb3acff619a9
Y Sawa <yoshihirosawa@gmail.com>
2012-12-21 17:56:49 +0900

    add: tags, its pagination and tests

--- spec/views/tags/index.html.haml_spec.rb
+++ spec/views/tags/index.html.haml_spec.rb
@@ -14,7 +14,7 @@ describe "tags/index.html.haml" do
     rendered.should match @tag.name
   end
 
-  describe 'ul.tags' do
+  describe 'ul#tags' do
     before :each do
       Tag.delete_all
       10.times do |i|
@@ -23,12 +23,38 @@ describe "tags/index.html.haml" do
       assign(:tags, Tag.page(1))
     end
 
-    it 'renders successfully' do
+    it 'contains tags' do
       render
-      rendered.should have_selector 'ul.tags'
-      assert_select 'ul.tags' do
+      rendered.should have_selector 'ul#tags'
+      assert_select 'ul#tags' do
         assert_select 'li.tag', 10
       end
+    end
+  end
+
+  describe 'nav.pagination' do
+    before :each do
+      Tag.delete_all
+      10.times do |i|
+        Fabricate(:tag, name: "Tag #{i}", code: "tag_#{i}")
+      end
+    end
+
+    it 'contains disabled pagination' do
+      assign(:tags, Tag.page(1))
+      render
+      rendered.should have_selector 'nav.pagination'
+      rendered.should have_selector 'nav.pagination span.disabled'
+    end
+
+    it 'contains enabled pagination' do
+      10.times do |i|
+        Fabricate(:tag, name: "Tag #{i + 10}", code: "tag_#{i + 10}")
+      end
+      assign(:tags, Tag.page(1))
+      render
+      rendered.should have_selector 'nav.pagination'
+      rendered.should have_selector 'nav.pagination a'
     end
   end
 
@@ -37,7 +63,7 @@ describe "tags/index.html.haml" do
       view.stub!(:params).and_return({ q: 'Key Word' })
     end
 
-    it 'renders successfully' do
+    it 'contains search field' do
       render
       rendered.should have_selector 'form#search_tag'
       rendered.should match 'Key Word'
