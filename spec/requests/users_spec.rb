2038025458aee75936e4dd341de1f63016a57f44
Y Sawa <yoshihirosawa@gmail.com>
2012-12-21 15:29:53 +0900

    modify: integration tests of signing in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,26 +8,23 @@ describe 'users' do
     @user = Fabricate(:user)
   end
 
-  context 'signing in' do
-    before :each do
+  describe "accessing /users/sign_in" do
+
+    it "page can be accessed" do
+      visit new_user_session_path
+      page.should have_selector 'form#user_sign_in'
     end
 
-    describe "accessing /users/sign_in" do
+    it "works successfully with correct password", js: true do
+      user_sign_in_with_visit(@user, 'PASSWORD')
+      page.should_not have_selector 'form#user_sign_in'
+      page.should have_content I18n.t('pages.controllers.home.mypage')
+    end
 
-      it "page can be accessed" do
-        visit new_user_session_path
-        page.should have_selector 'form#user_sign_in'
-      end
-
-      it "works successfully with correct password", js: true do
-        user_sign_in_with_visit(@user, 'PASSWORD')
-        page.should have_content I18n.t('pages.controllers.home.mypage')
-      end
-
-      it "fails with incorrect password", js: true do
-        user_sign_in_with_visit(@user, 'BADPASSWORD')
-        page.should have_selector 'form#user_sign_in'
-      end
+    it "fails with incorrect password", js: true do
+      user_sign_in_with_visit(@user, 'BADPASSWORD')
+      page.should have_selector 'form#user_sign_in'
+      page.should_not have_content I18n.t('pages.controllers.home.mypage')
     end
   end
 end
