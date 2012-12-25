fea17da88d41f3071f104bebb40d01b8fd5a3481
Y Sawa <yoshihirosawa@gmail.com>
2012-12-25 14:27:39 +0900

    modify: tests for pagination in sys/users/index

--- spec/views/sys/users/index.html.haml_spec.rb
+++ spec/views/sys/users/index.html.haml_spec.rb
@@ -13,4 +13,18 @@ describe "sys/users/index" do
     render
     rendered.should have_selector 'table.table'
   end
+
+  describe 'nav.pagination' do
+    before :each do
+      50.times do
+        Fabricate(:user)
+      end
+      assign(:users, User.all.page(1))
+    end
+
+    it "renders links" do
+      render
+      rendered.should have_selector 'nav.pagination'
+    end
+  end
 end
