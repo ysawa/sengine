7f0ec201b69ffdb23cf25e15ef5edd9a0529f036
Y Sawa <yoshihirosawa@gmail.com>
2012-12-25 15:38:34 +0900

    add: Tag#image

--- spec/views/tags/show.html.haml_spec.rb
+++ spec/views/tags/show.html.haml_spec.rb
@@ -13,4 +13,18 @@ describe "tags/show.html.haml" do
     render
     rendered.match @tag.name
   end
+
+  describe '.image' do
+    before :each do
+      @file = File.open(File.join(Rails.root, '/app/assets/images/rails.png'))
+      @tag.image = @file
+      @tag.save
+    end
+
+    it 'should be shown' do
+      render
+      p rendered
+      # .should have_selector('img')
+    end
+  end
 end
