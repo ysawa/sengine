760a632114378d6f1221d031fca8989e6bb9eed0
Y Sawa <yoshihirosawa@gmail.com>
2012-12-27 13:18:47 +0900

    add: tests for nav.pagination

--- spec/views/pushes/index.html.haml_spec.rb
+++ spec/views/pushes/index.html.haml_spec.rb
@@ -13,4 +13,11 @@ describe "pushes/index.html.haml" do
     render
     rendered.should have_content @push.content
   end
+
+  describe 'nav.pagination' do
+    it "renders a pagination" do
+      render
+      rendered.should have_selector 'nav.pagination'
+    end
+  end
 end
