6c4bd1ea85737967404f66dfbce952fa58b840d5
Y Sawa <yoshihirosawa@gmail.com>
2012-12-27 13:54:28 +0900

    add: tests for nav.pagination

--- spec/views/sys/pushes/index.html.haml_spec.rb
+++ spec/views/sys/pushes/index.html.haml_spec.rb
@@ -13,4 +13,18 @@ describe "sys/pushes/index.html.haml" do
     render
     rendered.should have_selector 'table.table'
   end
+
+  describe 'nav.pagination' do
+    before :each do
+      10.times do
+        @push = Fabricate(:push)
+      end
+      assign(:pushes, Push.all.page(1))
+    end
+
+    it "renders a pagination" do
+      render
+      rendered.should have_selector 'nav.pagination'
+    end
+  end
 end
