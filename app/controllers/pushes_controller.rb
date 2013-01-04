8a869551fca2d00a73f1b8b0e1d89583c296c2e7
Y Sawa <yoshihirosawa@gmail.com>
2013-01-04 14:03:55 +0900

    modify: hiding pushes

--- app/controllers/pushes_controller.rb
+++ app/controllers/pushes_controller.rb
@@ -4,10 +4,22 @@ class PushesController < ApplicationController
   respond_to :html, :json
 
   before_filter :authenticate_user!
+  before_filter :find_push, only: [:hide]
+
+  # PUT /pushes/1/hide
+  def hide
+    @push.hide_user!(current_user)
+  end
 
   # GET /pushes
   def index
     @pushes = Push.shown(current_user).page(params[:page])
     respond_with(@pushes)
+  end
+
+private
+
+  def find_push
+    @push = Push.find(params[:id])
   end
 end
